df0bbc384751eb57efd813bb68ba5ce8fbeab01b
as299999 <630777259@qq.com>
2023-08-31 23:59:27 +0800

    初始化地图

--- src/pages/Camera/index.ts
+++ src/pages/Camera/index.ts
@@ -1,11 +1,94 @@
-import { Cesium } from 'umi';
+import { Viewer, Camera, Cartesian3, Math, Ellipsoid } from 'cesium';
 
-export default class Camera {
-  viewer: Cesium.Viewer;
-  
-  constructor(
-    viewer: Cesium.Viewer
+interface CameraParamsType {
+  // degrees 摄像机观看方向
+  direction?: {
+    longitude?: number;
+    latitude?: number;
+    height?: number;
+  };
+  // degrees 摄像机所在位置
+  position?: {
+    longitude?: number;
+    latitude?: number;
+    height?: number;
+  };
+  // degrees
+  headingPitchRoll?: {
+    heading?: number;
+    pitch?: number;
+    roll?: number;
+  };
+}
+
+const defaultParams = {
+  direction: {
+    height: -6373460.407714644,
+    longitude: -76.0446209963375,
+    latitude: -27.977613191730875,
+  },
+  position: {
+    height: 26887007.08254164,
+    longitude: 103.95537900366243,
+    latitude: 27.66741874243775,
+  },
+  headingPitchRoll: {
+    heading: 360,
+    pitch: -89.97713228469502,
+    roll: 0,
+  },
+};
+
+export default class CameraClass {
+  viewer: Viewer;
+  camera: Camera;
+  cameraParams: CameraParamsType;
+  constructor (
+    viewer: Viewer,
+    gui: dat.GUI,
+    cameraParams?: CameraParamsType,
   ) {
-    this.viewer = viewer
+    this.viewer = viewer;
+    this.camera = viewer.scene.camera;
+    this.cameraParams = { ...defaultParams, ...cameraParams };
+    this.setInit(gui);
+  }
+
+  // 设置初始化
+  setInit(gui: dat.GUI) {
+    this.setView(this.cameraParams);
+    this.setGui(gui, this.cameraParams);
+  }
+
+  // 设置相机参数
+  setView(cameraParams: any) {
+    console.log(cameraParams)
+    this.camera.setView({
+      destination: Cartesian3.fromDegrees(
+        cameraParams.position.longitude,
+        cameraParams.position.latitude,
+        cameraParams.position.height,
+        Ellipsoid.WGS84
+      ),
+      orientation: {
+        heading: Math.toRadians(cameraParams.headingPitchRoll.heading),
+        pitch: Math.toRadians(cameraParams.headingPitchRoll.pitch),
+        roll: Math.toRadians(cameraParams.headingPitchRoll.roll)
+      }
+    })
+  }
+
+  // 设置gui
+  setGui(gui: dat.GUI, cameraParams: any) {
+    let cameraFolder = gui.addFolder('Camera 摄像机');
+    cameraFolder.open();
+
+    let listen_value = cameraFolder
+      .add(cameraParams.position, 'longitude')
+      .name('longitude [经度]')
+      .min(-360)
+      .max(360)
+      .step(0.000001)
+
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { Viewer, Cartesian3, Ellipsoid, Math } from 'cesium';
 import * as dat from 'dat.gui';
 import styles from './index.less';
 
+import Camera from './Camera';
+
 
 const cameraParams = {
   position: {
@@ -21,21 +23,6 @@ const HomePage = () => {
 
   useEffect(() => {
     const viewer = new Viewer('cesiumContainer', {});
-    const camera = viewer.scene.camera;
-
-    // 设置相机信息
-    camera.setView({
-      destination: Cartesian3.fromDegrees(
-        cameraParams.position.longitude,
-        cameraParams.position.latitude,
-        cameraParams.position.height
-        ,Ellipsoid.WGS84),
-      orientation: {
-        heading: Math.toRadians(cameraParams.headingPitchRoll.heading),
-        pitch: Math.toRadians(cameraParams.headingPitchRoll.pitch),
-        roll: Math.toRadians(cameraParams.headingPitchRoll.roll)
-      }
-    })
 
     const gui = new dat.GUI({
       name: 'Cesium GUI',
@@ -45,19 +32,26 @@ const HomePage = () => {
     })
     gui.domElement.style = 'position:absolute;top:10px;left:10px;';
 
-    let cameraFolder = gui.addFolder('Camera');
-    cameraFolder.open();
+    const camera = new Camera(viewer, gui, {
+      position: {
+        height: 55871,
+        longitude: 113.976006,
+        latitude: 22.475603,
+      },
+      headingPitchRoll: {
+        heading: 360,
+        pitch: -89.897722,
+        roll: 0,
+      },
+    });
 
-    let listen_value = cameraFolder
-      .add(cameraParams.position, 'longitude')
-      .name('longitude [经度]')
-      .min(-360)
-      .max(360)
-      .step(0.000001)
     
-    listen_value.onChange((value) => {
-      console.log(cameraParams)
-    })
+
+
+    
+    // listen_value.onChange((value) => {
+    //   console.log(cameraParams)
+    // })
     
     
 
